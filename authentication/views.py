c054952ed53a099a4f8c83673478d070eb29e8df
Zhengkang Wu <2852341w@student.gla.ac.uk>
2024-02-24 13:37:54 +0000

    add resources and page jump test

--- authentication/views.py
+++ authentication/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def test(request):
-    return render(request, 'authentication/test.html')
+def login(request):
+    return render(request, 'authentication/login.html')
+
+
+def register(request):
+    return render(request, 'authentication/register.html')
