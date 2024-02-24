5554565411d24c3daf58e5a7bc19c2846f7341c7
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-02-24 09:48:53 +0000

    Update the urls.py

---
+++ tango_with_django_project/userAuthentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from userAuthentication import views
+
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    # Add other authentication-related paths here
+]
