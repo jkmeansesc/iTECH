23b5b3cf52d61b36a5864493a05ea77eb46627ad
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-02-29 18:25:17 +0000

    update the publish and index function

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-from .forms import BlogForm
 from .models import Blog
 from .forms import BlogForm
 
@@ -8,73 +7,46 @@ from .forms import BlogForm
 # from django.core.mail import send_mail
 # from .utils import send_mails
 
-
-"""
 def index(request):
     if request.method == "POST":
         form = BlogForm(request.POST)
         if form.is_valid():
-
-            form.save(commit=False)
-            context_dict = {"message": "have a good day"}
-            blogs = Blog.objects.all()
-            context_dict["blogs"] = blogs
-            form = BlogForm()
-            context_dict["form"] = form
-
-            # update the images
-            if 'image' in request.FILES:
-                form.image = request.FILES['image']
-
-            form.save()
-
-            return render(request, "blog/publish.html", context=context_dict)
+            form.save(commit=True)
         else:
             print(form.errors)
+
+        context_dict = {"message": "have a good day"}
+        # 得到所有的blogs
+        blogs = Blog.objects.all()
+        context_dict["blogs"] = blogs
+        # 将表单传递给模板
+        form = BlogForm()
+        context_dict["form"] = form
+
+        return render(request, "blog/index.html", context=context_dict)
 
     else:
 
         context_dict = {"message": "have a good day"}
+        # 得到所有的blogs
         blogs = Blog.objects.all()
         context_dict["blogs"] = blogs
+        # 将表单传递给模板
         form = BlogForm()
         context_dict["form"] = form
 
-        return render(request, "blog/publish.html", context=context_dict)
-"""
+        return render(request, "blog/index.html", context=context_dict)
 
 def publish(request):
+
     if request.method == "POST":
         form = BlogForm(request.POST)
         if form.is_valid():
-
             form.save(commit=False)
-            context_dict = {"message": "have a good day"}
-            blogs = Blog.objects.all()
-            context_dict["blogs"] = blogs
-            form = BlogForm()
-            context_dict["form"] = form
+            return render(request, "blog/publish.html", context={"form": form})
 
-            # update the images
-            if 'image' in request.FILES:
-                form.image = request.FILES['image']
-
-            form.save()
-
-            return render(request, "blog/publish.html", context=context_dict)
         else:
             print(form.errors)
-
-    else:
-
-        context_dict = {"message": "have a good day"}
-        blogs = Blog.objects.all()
-        context_dict["blogs"] = blogs
-        form = BlogForm()
-        context_dict["form"] = form
-
-        return render(request, "blog/publish.html", context=context_dict)
-
 
 def about(request):
     return render(request, 'blog/about.html')
@@ -91,5 +63,3 @@ def blog_detail(request):
 def search_results(request):
     return render(request, 'blog/search_results.html')
 
-def index(request):
-    return render(request, 'base.html')
