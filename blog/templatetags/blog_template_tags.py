3107efc2383f2ab91324ea0a77fdd4aee9e712bf
Jing Yang <valianna000@gmail.com>
2024-03-02 23:11:22 +0000

    update base and template tags

--- blog/templatetags/blog_template_tags.py
+++ blog/templatetags/blog_template_tags.py
@@ -6,4 +6,4 @@ register = template.Library()
 
 @register.inclusion_tag('blog/smallBlogs.html')
 def get_blogs_list():
-    return {'blogs': Blog.objects.all()}
+    return {'blogs_latest': Blog.objects.order_by("-date")[:6]}
