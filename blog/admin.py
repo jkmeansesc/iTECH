bab3c763675568fdd349cbd5d427255c1e8628f0
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-03-02 22:05:22 +0000

    add url to blog

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,6 @@ from .models import Blog
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'tag', 'content', 'comment_num', 'image', 'date')
+    prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.template.defaultfilters import slugify
 
 class Blog(models.Model):
 
@@ -9,6 +10,12 @@ class Blog(models.Model):
     comment_num = models.IntegerField(default=0)
     image = models.ImageField(upload_to='blog_images', blank=True)
     date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, default='')
+
+    def save(self, *args, **kwargs):
+        #
+        self.slug = slugify(self.title)
+        super(Blog, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.content
