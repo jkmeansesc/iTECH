d1aabf8c930f2303214f0ce1a84f777f78b02794
Jing Yang <valianna000@gmail.com>
2024-03-05 21:15:20 +0000

    add profile_blogs.html and profile_settings.html, update app.css and views

--- blog/models.py
+++ blog/models.py
@@ -2,18 +2,17 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from django.template.defaultfilters import slugify
 
-class Blog(models.Model):
 
+class Blog(models.Model):
     title = models.CharField(max_length=200, default='')
     tag = models.CharField(max_length=200, default='')
     content = RichTextField(default="Please input your content here")
     comment_num = models.IntegerField(default=0)
     image = models.ImageField(upload_to='blog_images', blank=True)
-    date = models.DateTimeField(auto_now_add=True)    
+    date = models.DateTimeField(auto_now_add=True)
     # 作者, 作者是一个外键，指向用户表
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, default='')
     slug = models.SlugField(unique=True)
-
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
