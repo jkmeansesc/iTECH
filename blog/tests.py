0db6b22bebedd4e8eaa6162b68c2b497e9cd0bde
minicoderwen <373441770@qq.com>
2024-03-13 00:16:03 +0000

    update unit test

--- blog/tests.py
+++ blog/tests.py
@@ -1,8 +1,14 @@
+from unittest.mock import patch
+
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.mail import EmailMessage
+from django.test import Client, TestCase
+from django.urls import reverse
 
 from authentication.models import UserProfile
 
+from .forms import BlogForm
 from .models import Blog, Comment, Subscribe
 
 
@@ -65,3 +71,46 @@ class SubscribeTestCase(TestCase):
 
     def test_str_returned_correctly(self):
         self.assertEquals(str(self.test_subscription), "testuser subscribed testuser2")
+
+
+class IndexViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.index_url = reverse("index")
+        self.test_user = User.objects.create_user(
+            "testuser", "test@test.com", "testpass"
+        )
+        self.test_user_profile = UserProfile.objects.create(user=self.test_user)
+
+    def test_blog_form_GET(self):
+        response = self.client.get(self.index_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/index.html")
+        self.failUnless(isinstance(response.context["form"], BlogForm))
+
+    @patch("django.core.mail.send_mail")
+    def test_publish_POST_adds_blog_post(self, mocked_send_mail):
+        # create a simple mock image file
+        image_mock = SimpleUploadedFile(
+            name="test_image.png",
+            content=open("static/image/1.png", "rb").read(),
+            content_type="image/png",
+        )
+        self.client.force_login(self.test_user)
+        response = self.client.post(
+            self.index_url,
+            {
+                "title": "Test title",
+                "tag": "Test tag",
+                "content": "Test content",
+                "image": image_mock,
+            },
+        )
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(Blog.objects.first().title, "Test title")
+
+        # Verify that a one off email has been sent.
+        self.assertTrue(mocked_send_mail.called)
+        self.assertEquals(mocked_send_mail.call_count, 1)
